5e805414dc7955ac76511f9cccc29dc279f90145
SollWar <sollwarmorgrind@gmail.com>
2022-04-02 17:34:41 +0300

    Ignor

--- app/src/main/java/com/example/sollwar/bustickets/postgredb/AuthDate.kt
+++
@@ -1,7 +0,0 @@
-package com.example.sollwar.bustickets.postgredb
-
-data class AuthDate(
-    var user: String = "Sollwar",
-    val password: String = "548639909242aA",
-    val url: String = "jdbc:postgresql://rc1b-eolhpryga634iefm.mdb.yandexcloud.net:6432/Z3RG_BD"
-)
